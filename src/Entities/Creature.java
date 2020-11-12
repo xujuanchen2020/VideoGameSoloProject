a5eb46f33565fe01ab4ab3b2aceb4c01e8875ec6
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-11-12 17:41:01 -0500

    creature resizing

--- src/Entities/Creature.java
+++ src/Entities/Creature.java
@@ -3,9 +3,57 @@ package Entities;
 public abstract class Creature extends Entity {
 
     protected int health;
-    public Creature(double x, double y) {
-        super(x, y);
-        health = 10;
+    protected double speed;
+    protected double xMove, yMove;
+
+    public double getyMove() {
+        return yMove;
+    }
+
+    public static final int DEFAULT_HEALTH = 10;
+    public static final double DEFAULT_SPEED = 3.0;
+    public static final int DEFAULT_CREATURE_WIDTH = 64, DEFAULT_CREATURE_HEIGHT = 64;
+
+    public Creature(double x, double y, int width, int height) {
+        super(x, y, width, height);
+        health = DEFAULT_HEALTH;
+        speed = DEFAULT_SPEED;
+        xMove = 0;
+        yMove = 0;
+    }
+
+    public void move() {
+        x += xMove;
+        y += yMove;
+    }
+    // getters and setters
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+
+    public void setxMove(double xMove) {
+        this.xMove = xMove;
+    }
+
+    public void setyMove(double yMove) {
+        this.yMove = yMove;
+    }
+
+    public double getxMove() {
+        return xMove;
     }
 
 }
--- src/Entities/Entity.java
+++ src/Entities/Entity.java
@@ -4,10 +4,45 @@ import java.awt.*;
 
 public abstract class Entity {
     protected double x, y;
+    protected int width, height;
 
-    public Entity(double x, double y) {
+    public Entity(double x, double y, int width, int height) {
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     public abstract void tick();
