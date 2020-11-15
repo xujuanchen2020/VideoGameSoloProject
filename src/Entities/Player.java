c3b8c8d33e95e3e32bf163629b0f17d54b0cfcaf
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-11-15 15:08:30 -0500

    add Animation

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -1,24 +1,35 @@
 package Entities;
 
+import Gfx.Animation;
 import Gfx.Assets;
-import TileGame.Game;
 import TileGame.Handler;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Player extends Creature {
-
+    private Animation animationDown, animationUp, animationLeft, animationRight;
     public Player(Handler handler, double x, double y) {
         super(handler, x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
+        bounds.x = 22;
+        bounds.y = 44;
+        bounds.width = 19;
+        bounds.height =19;
 
-        bounds.x = 16;
-        bounds.y = 32;
-        bounds.width = 32;
-        bounds.height =32;
+        //animation
+        animationDown = new Animation(500,Assets.player_down);
+        animationUp = new Animation(500,Assets.player_up);
+        animationLeft = new Animation(500,Assets.player_left);
+        animationRight = new Animation(500,Assets.player_right);
     }
 
     @Override
     public void tick() {
+        animationDown.tick();
+        animationUp.tick();;
+        animationLeft.tick();
+        animationRight.tick();
+
         getInput();
         move();
         handler.getGameCamera().centerOnEntity(this);
@@ -44,7 +55,7 @@ public class Player extends Creature {
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.player,
+        g.drawImage(getCurrentAnimationFrame(),
                 (int)(x - handler.getGameCamera().getxOffset()),
                 (int)(y - handler.getGameCamera().getyOffset()),
                 width, height,null);
@@ -54,5 +65,17 @@ public class Player extends Creature {
 //                (int)(y + bounds.y - handler.getGameCamera().getyOffset()),
 //                bounds.width,
 //                bounds.height);
+    }
+
+    private BufferedImage getCurrentAnimationFrame() {
+        if(xMove < 0) {
+            return animationLeft.getCurrentFrame();
+        }else if (xMove > 0) {
+            return animationRight.getCurrentFrame();
+        }else if (yMove < 0) {
+            return animationUp.getCurrentFrame();
+        }else {
+            return animationDown.getCurrentFrame();
+        }
     }
 }
--- src/Gfx/Assets.java
+++ src/Gfx/Assets.java
@@ -3,18 +3,35 @@ package Gfx;
 import java.awt.image.BufferedImage;
 
 public class Assets {
-	
-//	private static final int width = 32, height = 32;
-	
+	private  static final int width = 64, height = 64;
 	public static BufferedImage player, dirt, grass, stone, tree;
-	
+	public static BufferedImage[] player_down, player_up, player_left, player_right;
+
 	public static void init() {
 		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/zombie.jpg"));
 		SpriteSheet sheetObject = new SpriteSheet(ImageLoader.loadImage("/textures/object.jpg"));
 		SpriteSheet sheetSprite2 = new SpriteSheet(ImageLoader.loadImage("/textures/sprite2.jpg"));
-		player = sheet.crop(125, 510, 44, 74);
 		dirt = sheet.crop(0, 0, 32, 32);
 		grass = sheetObject.crop(200, 300, 64, 64);
 		stone = sheetSprite2.crop(8,165,64,64);
+
+		player_down = new BufferedImage[2];
+		player_up = new BufferedImage[2];
+		player_left = new BufferedImage[2];
+		player_right = new BufferedImage[2];
+
+		player_down[0] = sheet.crop(55, 510, 44, 74);
+		player_down[1] = sheet.crop(125, 510, 44, 74);
+
+		player_up[0] = sheet.crop(195, 510, 44, 74);
+		player_up[1] = sheet.crop(265, 510, 44, 74);
+
+		player_left[0] = sheet.crop(335, 510, 44, 74);
+		player_left[1] = sheet.crop(405, 510, 44, 74);
+
+		player_right[0] = sheet.crop(475, 510, 44, 74);
+		player_right[1] = sheet.crop(545, 510, 44, 74);
+
 	}
+
 }
